1b3d1bdbca8cc4a318043a01d627cf5b2b0874d0
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-01-23 11:32:15 -0500

    added changes to server & db made changes for heroku

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Note Taker Starter Code
+# Note Taker Application
+
+![badge](https://img.shields.io/badge/license-MIT-orange)
+
+
+## Description
+created an application called Note Taker that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.
+
+## Deploying
+It has been deployed using [Heroku](https://.herokuapp.com/notes) 
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Screenshoot](#screenshoot)
+* [License](#license)
+* [Questions](#questions)
+
+## Installation
+* Heroku: You can access the application in the easiest way using the [Heroku](https://.herokuapp.com/notes)
+
+* Localhost: First, you have to download the files to run on your local machine. Secondly, use 'npm start' to initialize the port. After that, the app can be accessed in your browser by navigating to ''http://localhost:3001/'' 
+
+## Screenshoot
+![1](./img/1.png)
+![2](./img/2.png)
+
+## License
+![badge](https://img.shields.io/badge/license-MIT-orange)
+   
+Copyright (c) [2022] [Joseph Mogavero]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,7 +3,7 @@ let noteText;
 let saveNoteBtn;
 let newNoteBtn;
 let noteList;
-
+// If we are on the notes page, get references to the note title and text inputs and the save button
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
@@ -24,15 +24,15 @@ const hide = (elem) => {
 
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
-
-const getNotes = () =>
+// A function for getting all notes from the db
+const getNotes = () => 
   fetch('/api/notes', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
-  });
-
+  })
+// Save a new note to the db and update the view
 const saveNote = (note) =>
   fetch('/api/notes', {
     method: 'POST',
@@ -41,7 +41,7 @@ const saveNote = (note) =>
     },
     body: JSON.stringify(note),
   });
-
+// Delete a note
 const deleteNote = (id) =>
   fetch(`/api/notes/${id}`, {
     method: 'DELETE',
@@ -49,10 +49,10 @@ const deleteNote = (id) =>
       'Content-Type': 'application/json',
     },
   });
-
+// If there is an activeNote, display it, otherwise render empty inputs
 const renderActiveNote = () => {
   hide(saveNoteBtn);
-
+// If there is an activeNote, display it, otherwise render empty inputs
   if (activeNote.id) {
     noteTitle.setAttribute('readonly', true);
     noteText.setAttribute('readonly', true);
@@ -65,7 +65,7 @@ const renderActiveNote = () => {
     noteText.value = '';
   }
 };
-
+// Get the note data from the inputs, save it to the db and update the view
 const handleNoteSave = () => {
   const newNote = {
     title: noteTitle.value,
@@ -79,12 +79,12 @@ const handleNoteSave = () => {
 
 // Delete the clicked note
 const handleNoteDelete = (e) => {
-  // Prevents the click listener for the list from being called when the button inside of it is clicked
+  // prevents the click listener for the list from being called when the button inside of it is clicked
   e.stopPropagation();
-
+// Gets the id of the note to be deleted
   const note = e.target;
   const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
-
+// If we are on the notes page, delete the note with the given id
   if (activeNote.id === noteId) {
     activeNote = {};
   }
@@ -102,12 +102,13 @@ const handleNoteView = (e) => {
   renderActiveNote();
 };
 
-// Sets the activeNote to and empty object and allows the user to enter a new note
+// Sets the activeNote to an empty object and allows the user to enter a new note
 const handleNewNoteView = (e) => {
+  e.preventDefault();
   activeNote = {};
   renderActiveNote();
 };
-
+// If a note's title or text are empty, hide the save button
 const handleRenderSaveBtn = () => {
   if (!noteTitle.value.trim() || !noteText.value.trim()) {
     hide(saveNoteBtn);
@@ -122,21 +123,20 @@ const renderNoteList = async (notes) => {
   if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
-
+  // Stores the list items
   let noteListItems = [];
 
   // Returns HTML element with or without a delete button
   const createLi = (text, delBtn = true) => {
     const liEl = document.createElement('li');
     liEl.classList.add('list-group-item');
-
+    // Creates a span element to hold the note title
     const spanEl = document.createElement('span');
-    spanEl.classList.add('list-item-title');
     spanEl.innerText = text;
     spanEl.addEventListener('click', handleNoteView);
-
+    // Appends the span to the li
     liEl.append(spanEl);
-
+    // If delBtn is true, the li element will contain a button to delete the note
     if (delBtn) {
       const delBtnEl = document.createElement('i');
       delBtnEl.classList.add(
@@ -147,24 +147,24 @@ const renderNoteList = async (notes) => {
         'delete-note'
       );
       delBtnEl.addEventListener('click', handleNoteDelete);
-
+      // Appends the button to the li
       liEl.append(delBtnEl);
     }
-
+    // Returns the li element
     return liEl;
   };
-
+// If there are no notes, display a message saying so
   if (jsonNotes.length === 0) {
     noteListItems.push(createLi('No saved Notes', false));
   }
-
+//  Creates an li element for each note
   jsonNotes.forEach((note) => {
     const li = createLi(note.title);
     li.dataset.note = JSON.stringify(note);
 
     noteListItems.push(li);
   });
-
+// Appends the note list items to the list
   if (window.location.pathname === '/notes') {
     noteListItems.forEach((note) => noteList[0].append(note));
   }
@@ -172,7 +172,7 @@ const renderNoteList = async (notes) => {
 
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
-
+// If we are on the notes page, get and render the initial list of notes
 if (window.location.pathname === '/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
